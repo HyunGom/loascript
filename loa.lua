c6466b149af4bc5e8d1b8545a618412544d45433
Hyunook Kwon <hyunook23@gmail.com>
2022-12-03 08:30:55 -0500

    gem macro and some refactoring

--- loa.lua
+++ loa.lua
@@ -4,8 +4,12 @@ x,y = 1, 2
 LEFT_ALT = "lalt"
 ESC = "escape"
 ENTER = "enter"
+INVENTORY = "i"
+PET = "semicolon"
 
 
+
+-- MAIN FUNCTION
 function OnEvent(event, arg)
     -- arg 9 -> middle square button
     -- arg # -> mouse "G" button
@@ -14,64 +18,21 @@ function OnEvent(event, arg)
     --- uncomment to select a script to run ---
     -------------------------------------------
 --    check_coordinate()
+
 --    market(20, 6)
---    auction_gem(81 * 5)
 --    mail(8)
+
+
+--    auction_gem(81 * 5)
+--    gem_macro(3)
+
+
 --    hazardous()
 --    hazardous_repeat(1)
-      OutputLogMessage("DONE: %s\n", GetDate())
   end
 end
 
 
--- Mouse Left Click
-function MLC()
-    PressMouseButton(1)
-    Sleep(10 + math.random(10,20))
-    ReleaseMouseButton(1)
-    Sleep(10 + math.random(10,20))
-end
-
-
--- Mouse Right Click
-function MRC()
-    PressMouseButton(3)
-    Sleep(10 + math.random(10,20))
-    ReleaseMouseButton(3)
-    Sleep(10 + math.random(10,20))
-end
-
-
--- Move to (X,Y) and Left Click
-function left_click(x, y)
-    MoveMouseTo(x + math.random(10, 30), y + math.random(10, 30))
-    Sleep(100 + math.random(10,30))
-    MLC()
-    Sleep(150 + math.random(10,30))
-end
-
-
--- Move to (X,Y) and Right Click
-function right_click(x, y)
-    MoveMouseTo(x + math.random(10, 30), y + math.random(10, 30))
-    Sleep(100 + math.random(10,30))
-    MRC()
-    Sleep(150 + math.random(10,30))
-end
-
-
--- Keyboard Press
-function key(k)
-    PressKey(k)
-    Sleep(math.random(10,30))
-    ReleaseKey(k)
-end
-
-
--- Wait S second(s)
-function wait_second(s)
-    Sleep(s * 1000 + math.random(10, 30))
-end
 
 
 -- check mouse coordinate(x,y) and print them out
@@ -124,7 +85,6 @@ function market(n, r)
 end
 
 
-
 -- buy "n" number of gems
 function auction_gem(n)
   ClearLog()
@@ -151,13 +111,10 @@ function auction_gem(n)
       break
     end
     left_click(search[x], search[y])
-    wait_second(0.35)
+    wait_second(0.45)
     left_click(item[x], item[y])
-    wait_second(0.1)
     left_click(bidbuy[x], bidbuy[y])
-    wait_second(0.1)
     left_click(buynow[x], buynow[y])
-    wait_second(0.1)
     key(ENTER)
     wait_second(10)
   end
@@ -170,7 +127,7 @@ end
 -- retrieve all mails of "m" number of pages
 function mail(m)
   ClearLog()
-
+  
   -- select all check box
   select_x = 13750
   select_y = 19000
@@ -182,6 +139,13 @@ function mail(m)
   -- delete selection button
   delete_x = 26150
   delete_y = 50500
+  
+  remote_btn = {39800, 42700}
+  
+  key(PET)
+  wait_second(0.5)
+  left_click(remote_btn[x], remote_btn[y])
+  wait_second(1)
 
   for i=m, 1, -1
   do
@@ -193,6 +157,10 @@ function mail(m)
     key(ENTER)
     wait_second(0.5)
   end
+  
+  key(ESC)
+  key(ESC)
+  
 end
 
 
@@ -226,60 +194,6 @@ function hazardous()
   key(ESC)
 
 end
-
-function accept_guild()
-  quest_next = {31600, 47250}
-  guild_quest = {43950, 19800}
-  
-  -- open una task for guild quest
-  PressKey(LEFT_ALT)
-  key("j")
-  ReleaseKey(LEFT_ALT)
-  wait_second(0.8)
-  left_click(quest_next[x], quest_next[y])
-  wait_second(0.8)
-  left_click(guild_quest[x], guild_quest[y])
-  wait_second(1)
-  key(ESC)
-end
-
-function complete_quest()
-  check_button = {56500, 23800}
-  complete_button = {12150, 45000}
-  
-  left_click(check_button[x], check_button[y])
-  wait_second(0.5)
-  left_click(complete_button[x], complete_button[y])
-  wait_second(0.5)
-end
-
-
-function repair_ship()
-  map = {27000, 30000}
-  harbor = {46800, 33400}
-  repair_button = {38000, 61800}
-  
-  key("m")
-  wait_second(1)
-  
-  left_click(map[x], map[y])
-  wait_second(0.5)
-  
-  PressKey(LEFT_ALT)
-  left_click(harbor[x], harbor[y])
-  ReleaseKey(LEFT_ALT)
-  wait_second(45)
-  
-  key("z")
-  wait_second(3)
-  left_click(repair_button[x], repair_button[y])
-  wait_second(1)
-  key(ENTER)
-  wait_second(1)
-  key(ESC)
-  wait_second(10)
-end
-
 
 
 -- weekly guild quest hazardous REPEAT VER
@@ -354,4 +268,141 @@ function hazardous_repeat(a)
   end
   OutputLogMessage("FINISHED: %s\n", GetDate())
 
+end
+
+
+function gem_macro(n)
+
+  for i=n, 1, -1
+  do
+    mail(8)
+    wait_second(1)
+    fuse_gem()
+  end
+end
+  
+  
+------------------------ HELPER FUNCTIONS -------------------------------------
+-- Mouse Left Click
+function MLC()
+    PressMouseButton(1)
+    Sleep(10 + math.random(10,20))
+    ReleaseMouseButton(1)
+    Sleep(10 + math.random(10,20))
+end
+
+
+-- Mouse Right Click
+function MRC()
+    PressMouseButton(3)
+    Sleep(10 + math.random(10,20))
+    ReleaseMouseButton(3)
+    Sleep(10 + math.random(10,20))
+end
+
+
+-- Move to (X,Y) and Left Click
+function left_click(x, y)
+    MoveMouseTo(x + math.random(10, 30), y + math.random(10, 30))
+    Sleep(100 + math.random(10,30))
+    MLC()
+    Sleep(250 + math.random(10,30))
+end
+
+
+-- Move to (X,Y) and Right Click
+function right_click(x, y)
+    MoveMouseTo(x + math.random(10, 30), y + math.random(10, 30))
+    Sleep(100 + math.random(10,30))
+    MRC()
+    Sleep(250 + math.random(10,30))
+end
+
+
+-- Keyboard Press
+function key(k)
+    PressKey(k)
+    Sleep(math.random(10,30))
+    ReleaseKey(k)
+    Sleep(200 + math.random(10,30))
+end
+
+
+-- Wait S second(s)
+function wait_second(s)
+    Sleep(s * 1000 + math.random(10, 30))
+end
+
+
+function accept_guild()
+  quest_next = {31600, 47250}
+  guild_quest = {43950, 19800}
+  
+  -- open una task for guild quest
+  PressKey(LEFT_ALT)
+  key("j")
+  ReleaseKey(LEFT_ALT)
+  wait_second(0.8)
+  left_click(quest_next[x], quest_next[y])
+  wait_second(0.8)
+  left_click(guild_quest[x], guild_quest[y])
+  wait_second(1)
+  key(ESC)
+end
+
+function complete_quest()
+  check_button = {56500, 23800}
+  complete_button = {12150, 45000}
+  
+  left_click(check_button[x], check_button[y])
+  wait_second(0.5)
+  left_click(complete_button[x], complete_button[y])
+  wait_second(0.5)
+end
+
+
+function repair_ship()
+  map = {27000, 30000}
+  harbor = {46800, 33400}
+  repair_button = {38000, 61800}
+  
+  key("m")
+  wait_second(1)
+  
+  left_click(map[x], map[y])
+  wait_second(0.5)
+  
+  PressKey(LEFT_ALT)
+  left_click(harbor[x], harbor[y])
+  ReleaseKey(LEFT_ALT)
+  wait_second(45)
+  
+  key("z")
+  wait_second(3)
+  left_click(repair_button[x], repair_button[y])
+  wait_second(1)
+  key(ENTER)
+  wait_second(1)
+  key(ESC)
+  wait_second(10)
+end
+
+
+function fuse_gem()
+    gem_btn = {51350, 48950}
+    lv_btn = {36500, 44300}
+    fuse_btn = {41200, 48350}
+    confirm_btn = {31100, 35500}
+  
+  
+    key(INVENTORY)
+    left_click(gem_btn[x], gem_btn[y])
+    left_click(lv_btn[x], lv_btn[y])
+    left_click(fuse_btn[x], fuse_btn[y])
+    left_click(confirm_btn[x], confirm_btn[y])
+    wait_second(1)
+    key(ESC)
+    wait_second(0.3)
+    key(ESC)
+    wait_second(0.3)
 end
